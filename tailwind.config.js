a058c219a360af4852cf767b6ccbf3875a7452fe
Uggla <uggla@free.fr>
2025-08-04 16:29:09 +0200

    bkp commit

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{html,js}", "./templates/**/*.{html,js}"],
-  darkMode: "class",
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
